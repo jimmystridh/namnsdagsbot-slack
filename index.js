94f0917464c0891ff2e76f3d73190979d55f9d7d
Oskar Löfgren <islander.abroad@gmail.com>
2016-12-29 00:20:13 +0100

    Feature: Added name day reminders

--- index.js
+++ index.js
@@ -12,27 +12,52 @@ const slack = new SlackWebhook(process.env.WEBHOOK_URL, {
   }
 })
 
-const pad = (number) => number < 10 ? '0' + number : number
+const pad = number => number < 10 ? '0' + number : number
+const listToEnumerationText = list => list.length === 1
+  ? list[0].toString()
+  : `${list.slice(0, -1).join(', ')} och ${list[list.length - 1]}`
 
-const todayDate = new Date()
-const todayDateString = `${pad(todayDate.getMonth() + 1)}-${pad(todayDate.getDate())}`
-const nameDay = nameDays.find(day => day.date === todayDateString)
-if (!nameDay) return
+sendGratzToNameDayCelebrators()
+sendReminderToUpcomingCelebrators()
 
-nameDay.names.forEach(name => {
-  const todayNameDayUsers = nameToUsersMap[name.toLowerCase()]
-  if (todayNameDayUsers) {
-    const mentions = todayNameDayUsers.map(u => '@' + u)
-    const mentionsEnumerated = listToEnumerationText(mentions)
-    const gratz = `Idag gratulerar vi ${mentionsEnumerated} på ${name}-dagen :cake:`
+function sendGratzToNameDayCelebrators() {
+  const todayDate = new Date()
+  const todayDateString = `${pad(todayDate.getMonth() + 1)}-${pad(todayDate.getDate())}`
+  const nameDay = nameDays.find(day => day.date === todayDateString)
+  if (!nameDay) return
 
-    console.log(gratz)
-    slack.send({ text: gratz, parse: 'full' })
-  }
-})
+  nameDay.names.forEach(name => {
+    const todayNameDayUsers = nameToUsersMap[name.toLowerCase()]
+    if (todayNameDayUsers) {
+      const mentions = todayNameDayUsers.map(u => '@' + u)
+      const mentionsEnumerated = listToEnumerationText(mentions)
+      const gratz = `Idag gratulerar vi ${mentionsEnumerated} på ${name}-dagen :cake:`
 
-function listToEnumerationText(list) {
-  if (list.length === 1) return list[0].toString()
-
-  return `${list.slice(0, -1).join(', ')} och ${list[list.length - 1]}`
+      console.log(gratz)
+      slack.send({ text: gratz, parse: 'full' })
+    }
+  })
 }
+
+function sendReminderToUpcomingCelebrators() {
+  const tomorrowDate = new Date()
+  tomorrowDate.setDate(tomorrowDate.getDate() + 1)
+  const tomorrowDateString = `${pad(tomorrowDate.getMonth() + 1)}-${pad(tomorrowDate.getDate())}`
+  const nameDay = nameDays.find(day => day.date === tomorrowDateString)
+  if (!nameDay) return
+
+  nameDay.names.forEach(name => {
+    const tomorrowNameDayUsers = nameToUsersMap[name.toLowerCase()]
+    if (tomorrowNameDayUsers) {
+      tomorrowNameDayUsers.forEach(u => {
+        const mention = '@' + u
+        const reminder = ':bell: Imorgon har du namnsdag! Du glömmer väl inte fika? :cake: ' +
+          'Det är ju alltid uppskattat bland kollegorna :yum:'
+
+        console.log(reminder)
+        slack.send({ text: reminder, channel: mention })
+      })
+    }
+  })
+}
+
